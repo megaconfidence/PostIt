fba8c4bcab62ca0994937d6a148a2a3cf9e784b1
Confidence Okoghenun <confidenceboi@gmail.com>
2018-06-05 23:51:41 +0100

    Group Chat Page
    This commit includes the group chat and
    message notification board.
    
    *The groupchat.html was created and styled,
     also the chat-nav is hidden from view port
     below 880px.
    *This template has made it possible for user
     to send group messages.
    *Next up would be bug fixes, performance
     improvement and inclusion of skipped pages.
    
    [Closes-Bug: #003]

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
   $('.sidenav').sidenav();
   $(".dropdown-trigger").dropdown();
+  $('input.autocomplete').autocomplete({
+    data: {
+      "Apple": null,
+      "Microsoft": null,
+      "Google": 'https://placehold.it/250x250'
+    },
+  });
 });
+
+let viewPort = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+if(viewPort <= 880) {
+  console.log(viewPort);
+  let chatBar = document.querySelector('#chat-bar');
+  chatBar.style.display = 'none';
+  console.log(chatBar);
+  
+  let chatArea = document.querySelector('#chat-area');
+  chatArea.setAttribute('class', 'm12 col s12 grey lighten-3');
+  
+}
